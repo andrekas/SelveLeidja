0c9e29dec304acc498b4d5701afc20d7f7ebf1f6
andrekas <alaurimaa@gmail.com>
2018-02-21 14:31:35 +0200

    lisatud uued lehed ja menüü töötab

---
+++ src/meist.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Rühm</h2>
+                <p>Andre</p>
+                <p>Martin</p>
+                <p>Mark</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/pesulad.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Pesulad extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Pesulate nimekiri</h2>
+
+                <ol>
+                    <li>Mündipesula</li>
+                    <li>Jazz selvepsula</li>
+                    <li>SmartWash</li>
+                    <li>Isepesula24</li>
+                    <li>jne..</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default Pesulad;
