26f2e47c12b18e1d0a7f5a36f930d08aa077fd66
Martin <amormartin2@gmail.com>
2018-03-04 21:23:27 +0200

    Kommentaari input

--- src/pesulad.js
+++ src/pesulad.js
@@ -3,10 +3,13 @@ import './pesulad.css';
 
 class Pesulad extends Component {
     ShowDetails(event){
+       var comment =
+       "<Form><Input id='CommentInput' type='text' placeholder='Lisa kommentaar' class='unhidden'/></Form>";
+       var submitInput = "<Input id='SubmitInput' type='submit' value='Submit'/>";
        document.getElementById('DetailsHeading').innerHTML = event.currentTarget.textContent;
        document.getElementById('PriceDiv').innerHTML = "1€ minut";
        document.getElementById('LocationsDiv').innerHTML = "Siin ja seal";
-       document.getElementById('CommentDiv').innerHTML = "Parim pesula maailmas";
+       document.getElementById('CommentDiv').innerHTML = comment + submitInput;
     }
 
     render() {
@@ -15,22 +18,24 @@ class Pesulad extends Component {
             <div id="ContainerDiv">
                 <div id="ButtonsDiv">
                     <h2>Pesulad</h2>
-                    <a class='PesulaLink' href='#' onClick={this.ShowDetails}>Jazz Selvepesula</a><br/>
-                    <a class='PesulaLink' href='#' onClick={this.ShowDetails}>Mündipesula</a><br/>
-                    <a class='PesulaLink' href='#' onClick={this.ShowDetails}>SmartWash</a><br/>
-                    <a class='PesulaLink' href='#' onClick={this.ShowDetails}>NB Carwash. Self Wash</a><br/>
-                    <a class='PesulaLink' href='#' onClick={this.ShowDetails}>Kärcher Selvepesula</a><br/>
-                    <a class='PesulaLink' href='#' onClick={this.ShowDetails}>Isepesula 24</a><br/>
-                    <a class='PesulaLink' href='#' onClick={this.ShowDetails}>SV pesula</a><br/>
-                    <a class='PesulaLink' href='#' onClick={this.ShowDetails}>Aqua Pesulad OÜ</a><br/>
-                    <a class='PesulaLink' href='#' onClick={this.ShowDetails}>Wash24</a><br/>
-                    <a class='PesulaLink' href='#' onClick={this.ShowDetails}>Linnapesulad</a><br/>
+                    <a className='PesulaLink' onClick={this.ShowDetails}>Jazz Selvepesula</a><br/>
+                    <a className='PesulaLink' onClick={this.ShowDetails}>Mündipesula</a><br/>
+                    <a className='PesulaLink' onClick={this.ShowDetails}>SmartWash</a><br/>
+                    <a className='PesulaLink' onClick={this.ShowDetails}>NB Carwash. Self Wash</a><br/>
+                    <a className='PesulaLink' onClick={this.ShowDetails}>Kärcher Selvepesula</a><br/>
+                    <a className='PesulaLink' onClick={this.ShowDetails}>Isepesula 24</a><br/>
+                    <a className='PesulaLink' onClick={this.ShowDetails}>SV pesula</a><br/>
+                    <a className='PesulaLink' onClick={this.ShowDetails}>Aqua Pesulad OÜ</a><br/>
+                    <a className='PesulaLink' onClick={this.ShowDetails}>Wash24</a><br/>
+                    <a className='PesulaLink' onClick={this.ShowDetails}>Linnapesulad</a><br/>
                 </div>
                 <div id="DetailDiv">
                     <h2 id='DetailsHeading'>Siit leiad infot selvepesulate kohta</h2>
                     <div id="PriceDiv"></div>
                     <div id="LocationsDiv"></div>
-                    <div id="CommentDiv"></div>
+                    <div id="CommentDiv">
+                        <div id="CommentsDiv"></div>
+                    </div>
                 </div>
             </div>
         ]);
