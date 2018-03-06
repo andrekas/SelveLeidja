d0e3444d296977f6f70d00931c35a75b00ec0be7
Martin <amormartin2@gmail.com>
2018-03-06 10:43:24 +0200

    Meist tab

--- src/meist.js
+++ src/meist.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
+import './meist.css';
 
 class Contact extends Component {
     render() {
         return (
             <div>
-                <h2>Rühm</h2>
-                <p>Andre</p>
-                <p>Martin</p>
-                <p>Mark</p>
+                <h2>Meist</h2>
+                <p id="Tutvustus">Antud lehekülg on loodud Tarkvaratehnika aine raames. Rakenduse eesmärgiks on
+                                    koondada Tallinna selvepesulad ühte rakendusse. Mõnusat kasutamist!</p>
+                <footer>
+                    <p className="Autorid">Autorid:</p>
+                    <p className="Autorid">Andre Laurimaa, Martin Amor, Mark Luik</p>
+                </footer>
             </div>
         );
     }
