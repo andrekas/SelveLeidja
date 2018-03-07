49200463f0fbc5fee39be002ad9ede048d62e663
Martin <amormartin2@gmail.com>
2018-03-07 11:10:57 +0200

    Kommenteerimine

--- src/pesulad.js
+++ src/pesulad.js
@@ -2,16 +2,22 @@ import React from "react";
 import './pesulad.css';
 
 class Pesulad extends React.Component {
-    static ShowDetails(event){
-      let comment =
-          "<Form><Form.Field><input id='CommentInput' placeholder='Kommenteeri' /></Form.Field></Form>";
+      static ShowDetails(event){
+      let commentInput =
+          "<Form><Form.Field><input id='CommentInput' placeholder='Kommenteeri'/></Form.Field></Form>";
 
-      let submitInput = "<Button class='ui button' id='SubmitInput' type='submit'>Submit</Button>";
+      let submitInput =
+          "<button onClick={this.AddComment} class='ui button' id='SubmitButton' type='submit'>Esita</button>";
 
        document.getElementById('DetailsHeading').innerHTML = event.currentTarget.textContent;
        document.getElementById('PriceDiv').innerHTML = "1€ = 100 sekundit";
        document.getElementById('LocationsDiv').innerHTML = "Siin ja seal";
-       document.getElementById('CommentDiv').innerHTML = comment + submitInput;
+       document.getElementById('CommentElementsDiv').innerHTML = commentInput + submitInput;
+       document.getElementById('CommentsDiv').innerHTML = "Kommentaarid siia";
+    }
+    static AddComment(){
+       let comment = document.getElementById('CommentInput').value;
+       console.log(comment);
     }
 
     render() {
@@ -26,19 +32,18 @@ class Pesulad extends React.Component {
                     <a className='PesulaLink' onClick={Pesulad.ShowDetails} >NB Carwash</a><br/>
                     <a className='PesulaLink' onClick={Pesulad.ShowDetails} >Kärcher Selvepesula</a><br/>
                     <a className='PesulaLink' onClick={Pesulad.ShowDetails} >Isepesula 24</a><br/>
-                    <a className='PesulaLink' onClick={Pesulad.ShowDetails} >SV pesula</a><br/>
                     <a className='PesulaLink' onClick={Pesulad.ShowDetails} >Aqua Pesulad </a><br/>
                     <a className='PesulaLink' onClick={Pesulad.ShowDetails} >Wash24</a><br/>
                     <a className='PesulaLink' onClick={Pesulad.ShowDetails} >Linnapesulad</a><br/>
                     <a className='PesulaLink' onClick={Pesulad.ShowDetails} >Pese Ise</a><br/>
+                    <a className='PesulaLink' onClick={Pesulad.ShowDetails} >EHRLE Autopesula</a><br/>
                 </div>
                 <div id="DetailDiv">
                     <h2 id='DetailsHeading'>Siit leiad infot selvepesulate kohta</h2>
                     <div id="PriceDiv"></div>
                     <div id="LocationsDiv"></div>
-                    <div id="CommentDiv">
-                        <div id="CommentsDiv"></div>
-                    </div>
+                    <div id="CommentElementsDiv"></div>
+                    <div id="CommentsDiv"></div>
                 </div>
             </div>
         ]);
